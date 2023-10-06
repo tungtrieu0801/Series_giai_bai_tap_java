b4d8a92df90b62cd3dc0f58201f5b8a46114305f
tungtrieu0801 <trieutungvp@gmail.com>
2023-10-06 22:38:16 +0700

    bai_2

---
+++ bai_ 2__bo_ba_so.java
@@ -0,0 +1,15 @@
+package luyen_code;
+import java.util.Scanner;
+public class main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N, a,b,c;
+		N = sc.nextInt();
+		a = 0;
+		b = 2;
+		c = N-2;
+		N = a+ b+c;
+		System.out.println(a + " " + b + " "+c);
+
+	}
+}
